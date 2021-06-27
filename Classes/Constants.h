91fb1730fc014075bbd42a6557532ac4edc5f92c
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-06-27 14:37:59 +0300

    resources and path constants

---
+++ Classes/Constants.h
@@ -0,0 +1,37 @@
+//
+// Created by cetin on 27/06/2021.
+//
+
+#ifndef PROJ_ANDROID_CONSTANTS_H
+#define PROJ_ANDROID_CONSTANTS_H
+
+//
+// Fonts
+//
+
+#define PATH_FONT_FUTURE_THIN   "fonts/kenvector_future_thin.ttf"
+
+//
+// Images
+//
+
+#define PATH_IMG_AURA           "images/aura.png"
+#define PATH_IMG_BG             "images/bg.png"
+#define PATH_IMG_ENEMY          "images/enemy.png"
+#define PATH_IMG_HIT_GREEN      "images/hit_green.png"
+#define PATH_IMG_HIT_RED        "images/hit_red.png"
+#define PATH_IMG_LASER_GREEN    "images/laser_green.png"
+#define PATH_IMG_LASER_RED      "images/laser_red.png"
+#define PATH_IMG_PLAYER         "images/player.png"
+#define PATH_IMG_POWER_ICON     "images/power_icon.png"
+
+//
+// Sounds
+//
+
+#define PATH_SOUND_KILL         "sounds/kill.png"
+#define PATH_SOUND_LOSE         "sounds/lose.png"
+#define PATH_SOUND_POWER_DOWN   "sounds/power_down.png"
+#define PATH_SOUND_POWER_UP     "sounds/power_up.png"
+
+#endif //PROJ_ANDROID_CONSTANTS_H
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # space-invaders-clone
 A Space Invaders clone developed with Cocos2d-x for Android.
+
+## Third Party
+See [Acknowledgements.md](Acknowledgements.md) file for third party acknowledgements.
+
+## License
+MIT license. Refer to the [LICENSE](LICENSE) file.
